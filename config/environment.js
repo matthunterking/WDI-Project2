cf51293f594924eb582edc5d6a40f97fe20fc1f4
matthunterking <matthew85king@gmail.com>
2018-04-27 09:58:26 +0100

    Preparing for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-const databaseURI = 'mongodb://localhost/restaurant-review';
-const port = 3000;
+const databaseURI = process.env.MONGODB_URI || 'mongodb://localhost/restaurant-review';
+const port = process.env.PORT || 3000;
 
 module.exports = {databaseURI, port};
