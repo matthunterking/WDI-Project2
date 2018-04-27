c24bfea3a3ac7e42fe9aa6feb4724e912b69f79c
matthunterking <matthew85king@gmail.com>
2018-04-27 10:17:45 +0100

    Adding more dependencies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express             = require('express');
 const app                 = express();
-const bodyParser          = require('body-Parser');
+const bodyParser          = require('body-parser');
 const methodOverride      = require('method-override');
 const morgan              = require('morgan');
 const mongoose            = require('mongoose');
